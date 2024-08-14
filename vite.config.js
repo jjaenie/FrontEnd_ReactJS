5fcc44206eda0ee7b5ee14503276437c5da05e80
jjaenie <wogus990519@gmail.com>
2024-08-14 09:24:28 +0900

    react first commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+
+  server: {
+    port: 3000
+  },
+  //absolute path (절대경로)
+  resolve: {
+    alias: {
+      src: "/src",
+    }
+  },   
+})
